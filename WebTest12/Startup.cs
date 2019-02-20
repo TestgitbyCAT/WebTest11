b82f66e52ad55fde540cd8d7553173839fae4c22
TestgitbyCAT <testcat2019@gmail.com>
2019-02-20 10:28:52 +0700

    testCommit

---
+++ WebTest12/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebTest12.Startup))]
+namespace WebTest12
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
