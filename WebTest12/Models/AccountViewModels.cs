a06a8276315693844f780963223813830fa72b04
TestgitbyCAT <testcat2019@gmail.com>
2019-02-20 10:55:18 +0700

    loginBBB

--- WebTest12/Models/AccountViewModels.cs
+++ WebTest12/Models/AccountViewModels.cs
@@ -48,16 +48,17 @@ namespace WebTest12.Models
 
     public class LoginViewModel
     {
-        [Required]
+        //[Required]
         [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
-
+        [Display(Name = "User Name")]
+        [Required]
+        public string UserName { get; set; }
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
-
         [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
     }
@@ -68,13 +69,13 @@ namespace WebTest12.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
-
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
-
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
