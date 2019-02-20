f6fd8b135fa8c7d28048f62263f1d4a4bf94f3a0
TestgitbyCAT <testcat2019@gmail.com>
2019-02-20 11:29:51 +0700

    loginCCC

--- WebTest12/Models/AccountViewModels.cs
+++ WebTest12/Models/AccountViewModels.cs
@@ -71,6 +71,9 @@ namespace WebTest12.Models
         public string Email { get; set; }
         [Display(Name = "User Name")]
         public string UserName { get; set; }
+        [Display(Name = "Full Name"), Required]
+        public string FullName { get; set; }
+
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
@@ -80,6 +83,7 @@ namespace WebTest12.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+        public Genders Gender { get; set; }
     }
 
     public class ResetPasswordViewModel
